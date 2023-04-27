0226bd38000feda4b6d6e89e27f50c818ab09974
Karina Farias <113721566+Karina-Farias@users.noreply.github.com>
2023-04-27 16:45:21 -0300

    Add files via upload

---
+++ Informações.py
@@ -0,0 +1,4 @@
+Nome = input("Nome:")
+Idade = input("Idade:")
+Salario = float(input("Salário:"))
+print(f"Nome: {Nome}, Idade: {Idade}, Salário: {Salario}")
---
+++ OperaçõesMatematicas.py
@@ -0,0 +1,10 @@
+Numero1 = float(input("DIGITE UM NÚMERO:"))
+Numero2 = float(input("DIGITE OUTRO NÚMERO:"))
+Soma = Numero1 + Numero2
+print("SOMA:", Soma)
+Subtracao = Numero1 - Numero2
+print("SUBTRAÇÃO:",Subtracao)
+Multiplicacao = Numero1 * Numero2
+print("MULTIPLICAÇÃO:",Multiplicacao)
+Divisao = Numero1 / Numero2
+print("DIVISÃO:",Divisao)
